88b8c865b32a0c3630aba92256c1141c50c53c1e
dominikjaniga91 <dominikjaniga91@gmail.com>
2021-03-13 20:02:47 +0100

    Override equals and hashcode in Field class

--- src/main/java/game/board/Field.java
+++ src/main/java/game/board/Field.java
@@ -1,5 +1,7 @@
 package game.board;
 
+import java.util.Objects;
+
 /**
  * Represent the field in the battle board. Empty field is represented
  * byt '~' mark. The field that contains the part of ship is represented by '0'.
@@ -18,8 +20,33 @@ class Field {
         this.column = column;
     }
 
+    Field() {
+
+    }
+
     @Override
     public String toString() {
         return value;
+    }
+
+    public void markField() {
+        value = " 0";
+    }
+
+    boolean isEmpty() {
+        return !"0".equals(value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Field field = (Field) o;
+        return Objects.equals(value, field.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(column);
     }
 }
